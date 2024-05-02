dcef1b77a99a2ad1d75d4d6f3e8c6237657762e8
BigTava <bigtavadev@gmail.com>
2024-05-02 23:37:19 +0100

    wip: enables balance transfers

--- src/balances.rs
+++ src/balances.rs
@@ -19,6 +19,23 @@ impl Pallet {
     pub fn balance(&self, who: &String) -> u128 {
         *self.balances.get(who).unwrap_or(&0)
     }
+
+    pub fn transfer(
+        &mut self,
+        caller: &String,
+        to: &String,
+        amount: u128
+    ) -> Result<(), &'static str> {
+        let caller_balance = self.balance(caller);
+        let to_balance = self.balance(to);
+
+        let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Not enough funds.")?;
+        let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow.")?;
+
+        self.set_balance(&caller, new_caller_balance);
+        self.set_balance(&to, new_to_balance);
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -34,5 +51,20 @@ mod tests {
         balances.set_balance(&"alice".to_string(), 100);
         assert_eq!(balances.balance(&"alice".to_string()), 100);
         assert_eq!(balances.balance(&"bob".to_string()), 0);
+    }
+
+    #[test]
+    fn transfer_balance() {
+        let mut balances = Pallet::new();
+
+        assert_eq!(
+            balances.transfer(&"alice".to_string(), &"bob".to_string(), 100),
+            Err("Not enough funds.")
+        );
+
+        balances.set_balance(&"alice".to_string(), 100);
+        assert_eq!(balances.transfer(&"alice".to_string(), &"bob".to_string(), 50), Ok(()));
+        assert_eq!(balances.balance(&"alice".to_string()), 50);
+        assert_eq!(balances.balance(&"bob".to_string()), 50);
     }
 }
