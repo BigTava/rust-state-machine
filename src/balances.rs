13b34d91ecaf3574eb068a572a9fe759ec6c278f
BigTava <bigtavadev@gmail.com>
2024-05-02 19:37:26 +0100

    tests: basic balance

--- src/balances.rs
+++ src/balances.rs
@@ -13,10 +13,26 @@ impl Pallet {
     }
 
     pub fn set_balance(&mut self, who: &String, amount: u128) {
-        self.balances.insert(who.to_string(), amount);
+        self.balances.insert(who.clone(), amount);
     }
 
     pub fn balance(&self, who: &String) -> u128 {
         *self.balances.get(who).unwrap_or(&0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Pallet;
+
+    #[test]
+    fn init_balances() {
+        let mut balances = Pallet::new();
+
+        assert_eq!(balances.balance(&"alice".to_string()), 0);
+
+        balances.set_balance(&"alice".to_string(), 100);
+        assert_eq!(balances.balance(&"alice".to_string()), 100);
+        assert_eq!(balances.balance(&"bob".to_string()), 0);
     }
 }
