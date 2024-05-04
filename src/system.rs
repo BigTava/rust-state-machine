211139d281f1772159135e5b2c29c347681e7fb1
BigTava <bigtavadev@gmail.com>
2024-05-05 00:30:33 +0100

    wip: makes system functional

--- src/system.rs
+++ src/system.rs
@@ -12,4 +12,35 @@ impl Pallet {
             nonce: BTreeMap::new(),
         }
     }
+
+    pub fn block_number(&self) -> u32 {
+        self.block_number
+    }
+
+    pub fn inc_block_number(&mut self) {
+        self.block_number += 1;
+    }
+
+    // Increment the nonce of an account. This helps us keep track of how many transactions each
+    // account has made.
+    pub fn inc_nonce(&mut self, who: &String) {
+        let nonce = *self.nonce.get(who).unwrap_or(&0);
+        let new_nonce = nonce + 1;
+        self.nonce.insert(who.clone(), new_nonce);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::system::Pallet;
+
+    #[test]
+    fn init_system() {
+        let mut system = Pallet::new();
+        system.inc_block_number();
+        system.inc_nonce(&"alice".to_string());
+
+        assert_eq!(system.block_number(), 1);
+        assert_eq!(system.nonce.get(&"alice".to_string()), Some(&1));
+    }
 }
