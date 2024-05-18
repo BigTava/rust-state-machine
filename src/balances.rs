d9f31072e26a01787b102d5a5a9eb7ba0a09f910
BigTava <bigtavadev@gmail.com>
2024-05-18 11:58:55 +0400

    wip: adds Call macro to balances

--- src/balances.rs
+++ src/balances.rs
@@ -25,46 +25,24 @@ impl<T: Config> Pallet<T> {
     pub fn balance(&self, who: &T::AccountId) -> T::Balance {
         *self.balances.get(who).unwrap_or(&T::Balance::zero())
     }
+}
 
+#[macros::call]
+impl<T: Config> Pallet<T> {
     pub fn transfer(
         &mut self,
-        caller: &T::AccountId,
-        to: &T::AccountId,
+        caller: T::AccountId,
+        to: T::AccountId,
         amount: T::Balance
     ) -> crate::support::DispatchResult {
-        let caller_balance = self.balance(caller);
-        let to_balance = self.balance(to);
+        let caller_balance = self.balance(&caller);
+        let to_balance = self.balance(&to);
 
         let new_caller_balance = caller_balance.checked_sub(&amount).ok_or("Not enough funds.")?;
         let new_to_balance = to_balance.checked_add(&amount).ok_or("Overflow.")?;
 
         self.set_balance(&caller, new_caller_balance);
         self.set_balance(&to, new_to_balance);
-        Ok(())
-    }
-}
-
-pub enum Call<T: Config> {
-    Transfer {
-        to: T::AccountId,
-        amount: T::Balance,
-    },
-}
-
-impl<T: Config> crate::support::Dispatch for Pallet<T> {
-    type Caller = T::AccountId;
-    type Call = Call<T>;
-
-    fn dispatch(
-        &mut self,
-        caller: Self::Caller,
-        call: Self::Call
-    ) -> crate::support::DispatchResult {
-        match call {
-            Call::Transfer { to, amount } => {
-                self.transfer(&caller, &to, amount)?;
-            }
-        }
         Ok(())
     }
 }
@@ -100,12 +78,12 @@ mod tests {
         let mut balances = Pallet::<TestConfig>::new();
 
         assert_eq!(
-            balances.transfer(&"alice".to_string(), &"bob".to_string(), 100),
+            balances.transfer("alice".to_string(), "bob".to_string(), 100),
             Err("Not enough funds.")
         );
 
         balances.set_balance(&"alice".to_string(), 100);
-        assert_eq!(balances.transfer(&"alice".to_string(), &"bob".to_string(), 50), Ok(()));
+        assert_eq!(balances.transfer("alice".to_string(), "bob".to_string(), 50), Ok(()));
         assert_eq!(balances.balance(&"alice".to_string()), 50);
         assert_eq!(balances.balance(&"bob".to_string()), 50);
     }
